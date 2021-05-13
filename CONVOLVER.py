a141f9b9c5d71f63144ec2092f5202ccc70142b7
SmokeyJoe <josephcharlescooper@gmail.com>
2021-05-13 17:45:02 +0100

    yes

--- CONVOLVER.py
+++ CONVOLVER.py
@@ -10,16 +10,27 @@ filename = sys.argv[1]
 # Output data in form E (eV), wavelength (nm), Stick f, total f, f_1,
 # f_2...,f_n
 
+islorentzian = False  # set to True if you want a lorentzian
+
 data=np.genfromtxt(filename, delimiter=",")  # take in data from the first argument
 
 invtworoottwologtwo = 1/(2*np.sqrt(2*np.log(2))) # precompute the inverse of 2 root 2log2
 
-def eVtonm(energy): # function that returns the nm value of an energy in eV
-    nm=(299792458*4.135667516E-6)/(energy)
+def eVtonm(e): # function that returns the nm value of an energy in eV
+    nm=(299792458*4.135667516E-6)/(e)
     return nm
+
 def nmtoeV(wavelength): # function that returns the ev value of a wavelength in nm
     energy=(299792458*4.135667516E-6)/(wavelength)
     return energy
+
+def eVtoinvcm(energy):
+    return 8065.54429 * energy
+
+def lorentzian(x, height, centre, width):
+    gam=0.5*width
+    l=1/(np.pi*gam) * gam / ((x-centre)**2 + gam**2)
+    return l * height
 
 def gauss(x,height, centre, width): # returns the value of a gaussian of given height, centre, and width at c
     g=height*np.exp((-(x-centre)**2)/(2*(width**2)))
@@ -39,10 +50,6 @@ print("Minimum energy is "+str(emin)) # prints in eV
 FWHM = float(input("What FWHM (eV) do you want?\n")) # user input of FWHM
 samples = int(input("How many samples do you want?\n")) # user input of number of samples
 
-c = FWHMtoC(FWHM) # converts user input FWHM into the standard dev
-
-NonC = 11452.3149/c # L^3 mol^-1 cm ^ -1 = Na * e ^2 * h / (4*m_e*c*epsilon_0*ln(10)*sqrt(2pi)) # conversion factor you times by f/c
-
 noex = int(np.shape(data)[0]/2) # calculates number of excitations (exactly half the number of data points...)
 
 if np.shape(data)[0]%2 != 0: # very basic checking procedure
@@ -52,12 +59,7 @@ if np.shape(data)[0]%2 != 0: # very basic checking procedure
 spec = np.zeros((samples,noex+4)) # init the blank array of zeros
 
 spec[:,0] = np.linspace(emin,emax,samples) # create the linear space of energies between the extrema
-spec[:,1] = eVtonm(spec[:,0]) # convert this into the wavelength
-
-for i in range(noex): # for all excitations, calculate a gaussian of height of the oscillator strength centred at the energy, and put into column n=4
-    spec[:,i+4] = data[i+noex] * NonC * gauss(spec[:,0],data[i+noex],data[i],c)
-
-spec[:,3] = [np.sum(spec[i,4:]) for i in range(samples)] # sum over all the columns to get total spectrum
+spec[:,1] = [eVtonm(x) for x in spec[:,0]]
 
 for i in range(noex): # put in the stick spectrum in the column of the first value lower than the energy (becomes a better approx as samples increases)
     if data[i] > emin and data[i] < emax:
@@ -66,6 +68,21 @@ for i in range(noex): # put in the stick spectrum in the column of the first val
     else:
         print("Excitation "+str(i)+" not in range of plot!") # says if any of the excitations are not included in the plot
 
+if islorentzian:
 
-np.savetxt(filename+"data",spec,fmt='%.6e', delimiter='   ') # save the data.
+    N = 11452.3149
+
+    for i in range(noex): # for all excitations, calculate a gaussian of height of the oscillator strength centred at the energy, and put into column n=4
+        spec[:,i+4] = N * lorentzian(spec[:,0],data[i+noex],data[i],FWHM)
+
+else :
+    c = FWHMtoC(FWHM) # converts user input FWHM into the standard dev
+    NonC = 11452.3149/c # L^3 mol^-1 cm ^ -1 = Na * e ^2 * h / (4*m_e*c*epsilon_0*ln(10)*sqrt(2pi) * c) # conversion factor you times by f/c
+    for i in range(noex): # for all excitations, calculate a gaussian of height of the oscillator strength centred at the energy, and put into column n=4
+        spec[:,i+4] = NonC * gauss(spec[:,0],data[i+noex],data[i],c)
+
+
+spec[:,3] = [np.sum(spec[i,4:]) for i in range(samples)] # sum over all the columns to get total spectrum
+
+np.savetxt("Spectrum_"+filename,spec,fmt='%.6e', delimiter='   ') # save the data.
 
--- ricc2_EXTRACTOR.sh
+++ ricc2_EXTRACTOR.sh
@@ -5,10 +5,18 @@
 
 outfile=energies.out
 
+bool=`grep -c 'EXCITED STATE TRANSITION MOMENTS' $1`
+
+if [ $bool -eq 1 ]
+then
+grep -A1000 'I       F' $1 | grep 'diplen' | awk '{print $7}' > $outfile
+grep -A1000 'I       F' $1 | grep 'diplen' | awk '{print $11}' >> $outfile
+else
 nexc=`grep 'number of vectors generated' $1 | awk '{print $6}' | head -n1`
-
 exp3=`echo "$nexc + 3" | bc`
-
 grep -A${exp3} "ADC(2) excitation energies" $1 | tail -n$nexc | tr -s ' ' | cut -d ' ' -f11 >> $outfile
 grep 'oscillator strength (length gauge)' $1 | awk '{print $6}' >> $outfile
+fi
+
+
 sed -i ':a;N;$!ba;s/\n/,/g' $outfile
